9b8e6f5a328801630fc9da96f1b5faba4fea1578
Otstar Lin <syfxlin@gmail.com>
2019-02-14 14:47:52 +0800

    sw规则修复
    修改页面的缓存规则，将页面设为优先使用网络，确保页面能及时更新，并且防止因使用autoptimize插件而产生的一些问题

--- js/sw.js
+++ js/sw.js
@@ -10,33 +10,8 @@ if (workbox) {
 }
 
 // 定义缓存版本号和默认 Cache Storage 条目数
-let cacheSuffixVersion = '-181111';
+let cacheSuffixVersion = '-190214';
 const maxEntries = 100;
-
-workbox.routing.registerRoute(
-    /.*\?action.*/,
-    workbox.strategies.networkFirst()
-);
-
-workbox.routing.registerRoute(
-    /.*&action.*/,
-    workbox.strategies.networkOnly()
-);
-
-workbox.routing.registerRoute(
-    /.*wp-admin.*/,
-    workbox.strategies.networkOnly()
-);
-
-workbox.routing.registerRoute(
-    /.*sitemap.*/,
-    workbox.strategies.networkOnly()
-);
-
-workbox.routing.registerRoute(
-    /.*feed.*/,
-    workbox.strategies.networkOnly()
-);
 
 workbox.routing.registerRoute(
     // 使用正则表达式匹配路由
@@ -49,8 +24,8 @@ workbox.routing.registerRoute(
             new workbox.expiration.Plugin({
                 // 最大保存项目
                 maxEntries,
-                // 缓存 30 天
-                maxAgeSeconds: 30 * 24 * 60 * 60,
+                // 缓存 7 天
+                maxAgeSeconds: 7 * 24 * 60 * 60,
             }),
             // 使用 cacheableResponse 插件缓存状态码为 0 的请求
             new workbox.cacheableResponse.Plugin({
@@ -62,21 +37,11 @@ workbox.routing.registerRoute(
 
 workbox.routing.registerRoute(
     new RegExp('https://dev\.ixk\.me.*'),
-    workbox.strategies.staleWhileRevalidate({
-        cacheName: 'blog-cache' + cacheSuffixVersion,
-        plugins: [
-            // 使用 expiration 插件实现缓存条目数目和时间控制
-            new workbox.expiration.Plugin({
-                // 最大保存项目
-                maxEntries,
-                // 缓存 30 天
-                maxAgeSeconds: 3 * 24 * 60 * 60,
-            }),
-            // 使用 cacheableResponse 插件缓存状态码为 0 的请求
-            new workbox.cacheableResponse.Plugin({
-                statuses: [0, 200],
-            }),
-        ]
+    workbox.strategies.networkFirst({
+        options: [{
+            // 超过 3s 请求没有响应则 fallback 到 cache
+            networkTimeoutSeconds: 3,
+        }]
     })
 );
 
@@ -143,13 +108,45 @@ workbox.routing.registerRoute(
     })
 );
 
-workbox.routing.setDefaultHandler(
-    workbox.strategies.networkFirst({
-        options: [{
-            // 超过 3s 请求没有响应则 fallback 到 cache
-            networkTimeoutSeconds: 3,
-        }]
-    })
+workbox.routing.registerRoute(
+    /.*\?action.*/,
+    workbox.strategies.networkFirst()
 );
+
+workbox.routing.registerRoute(
+    /.*&action.*/,
+    workbox.strategies.networkOnly()
+);
+
+workbox.routing.registerRoute(
+    /.*wp-admin.*/,
+    workbox.strategies.networkOnly()
+);
+
+workbox.routing.registerRoute(
+    /.*wp-login.*/,
+    workbox.strategies.networkOnly()
+);
+
+workbox.routing.registerRoute(
+    /.*sitemap.*/,
+    workbox.strategies.networkOnly()
+);
+
+workbox.routing.registerRoute(
+    /.*feed.*/,
+    workbox.strategies.networkOnly()
+);
+
+workbox.routing.registerRoute(
+    /.*\.php/,
+    workbox.strategies.networkOnly()
+);
+
+workbox.routing.registerRoute(
+    /.*syfxlin.*/,
+    workbox.strategies.networkOnly()
+);
+
 workbox.skipWaiting();
 workbox.clientsClaim();
