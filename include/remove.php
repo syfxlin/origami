29f173c1dea6aec394f89d863434d44f43b36544
syfxlin <syfxlin@gmail.com>
2019-08-27 13:29:36 +0800

    fix: :bug: Remove jquery

--- include/remove.php
+++ include/remove.php
@@ -1,7 +1,7 @@
 <?php
 // 移除多余代码
-function remove_jquery()
-{
-  wp_deregister_script('jquery');
+add_filter( 'wp_enqueue_scripts', 'change_default_jquery', PHP_INT_MAX );
+function change_default_jquery( ){
+  wp_dequeue_script( 'jquery');
+  wp_deregister_script( 'jquery');
 }
-add_action('wp_footer', 'remove_jquery');
