d86caa471efe860bff967f454701085eb72d136b
syfxlin <syfxlin@gmail.com>
2019-08-27 13:33:58 +0800

    fix: :bug: Fix jquery backend load

--- include/remove.php
+++ include/remove.php
@@ -1,7 +1,9 @@
 <?php
 // 移除多余代码
-add_filter( 'wp_enqueue_scripts', 'change_default_jquery', PHP_INT_MAX );
-function change_default_jquery( ){
-  wp_dequeue_script( 'jquery');
-  wp_deregister_script( 'jquery');
+if (!is_admin()) {
+    add_filter( 'wp_enqueue_scripts', 'change_default_jquery', PHP_INT_MAX );
+    function change_default_jquery( ){
+         wp_dequeue_script( 'jquery');
+         wp_deregister_script( 'jquery'); 
+    }
 }
