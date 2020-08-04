55818c4a6739d7bdc9f702e543c3c0ceb903a0e6
Styunlen <30810222+Styunlen@users.noreply.github.com>
2020-08-04 17:58:14 +0800

    添加了多页文章的导航栏
    当文章过长时，文章可以使用wordpress自带的分页标签进行分页，而如果没有导航栏，wordpress默认只会显示多页文章中的第一页，因此添加分页导航栏来增加主题对分页文章的支持

--- functions.php
+++ functions.php
@@ -608,6 +608,70 @@ function origami_inspiration_init()
 }
 add_action('init', 'origami_inspiration_init');
 
+//文末文章分页显示
+  //wp_link_pages参数列表（复制粘贴大法）
+ /*$args {
+  *     Optional. Array or string of default arguments.
+  *
+  *     @type string       $before           HTML or text to prepend to each link. Default is `<p> Pages:`.
+  *     @type string       $after            HTML or text to append to each link. Default is `</p>`.
+  *     @type string       $link_before      HTML or text to prepend to each link, inside the `<a>` tag.
+  *                                          Also prepended to the current item, which is not linked. Default empty.
+  *     @type string       $link_after       HTML or text to append to each Pages link inside the `<a>` tag.
+  *                                          Also appended to the current item, which is not linked. Default empty.
+  *     @type string       $aria_current     The value for the aria-current attribute. Possible values are 'page',
+  *                                          'step', 'location', 'date', 'time', 'true', 'false'. Default is 'page'.
+  *     @type string       $next_or_number   Indicates whether page numbers should be used. Valid values are number
+  *                                          and next. Default is 'number'.
+  *     @type string       $separator        Text between pagination links. Default is ' '.
+  *     @type string       $nextpagelink     Link text for the next page link, if available. Default is 'Next Page'.
+  *     @type string       $previouspagelink Link text for the previous page link, if available. Default is 'Previous Page'.
+  *     @type string       $pagelink         Format string for page numbers. The % in the parameter string will be
+  *                                          replaced with the page number, so 'Page %' generates "Page 1", "Page 2", etc.
+  *                                          Defaults to '%', just the page number.
+  *     @type int|bool     $echo             Whether to echo or not. Accepts 1|true or 0|false. Default 1|true.
+  * }
+  * @return string Formatted output in HTML.
+  */
+function origami_content_pagesNav($content)
+{
+  //代码有点乱，QAQ
+  $content .= 
+  /* 上一页标签 */
+  wp_link_pages(
+    array('before' => '<div class="pagination" id="nav_articlePages"><span id="nav_page_title">分页</span>',
+    'after' => '',
+    'next_or_number' => 'next',
+    'link_before' =>'<span id="nav_previous_page">',
+    'link_after'=>'</span>',
+    'previouspagelink' => '←上一页',
+    'nextpagelink' => "",
+    'echo' => 0  
+    )).
+    /* 文章页码标签 */
+    wp_link_pages(array(
+    'before' => '',
+    'after' => '',
+    'next_or_number' => 'number',
+    'link_before' =>'<span class="nav_page_num">第',
+    'link_after'=>'页</span>',
+    'echo' => 0
+    )).
+    /* 下一页标签 */
+    wp_link_pages(array(
+    'before' => '',
+    'after' => '</div>',
+    'next_or_number' => 'next',
+    'previouspagelink' => '',
+    'link_before' =>'<span id="nav_next_page">',
+    'link_after'=>'</span>',
+    'nextpagelink' => "下一页→",
+    'echo' => 0 
+    )); 
+  return $content;
+}
+add_filter('the_content', 'origami_content_pagesNav');
+
 // 文末版权声明
 function origami_content_copyright($content)
 {
